4f41c6fdad5136d2d441af0ba8ba8723216b60d5
Mir <mirosul@gmail.com>
2013-07-30 03:06:53 +0300

    cleanups and todos

--- email-android/assets/www/js/index.js
+++ email-android/assets/www/js/index.js
@@ -1,42 +1,26 @@
 //00-splash,    01-tutorial1, 02-tutorial2,    03-tutorial3, 04-add email, 05-sync
 //              11-compose,   12-inbox,        13-sent,      14-trash,     15-archive, 16-settings
 //20-reply all, 21-reply,     22-read message, 23-forward 	 24-contacts
-
-var currentScreen = 0;
-var menus = ['splash' ,'tutorial page 1' ,'tutorial page 2' ,'tutorial page 3' ,'add email' ,'syncronization', '', '', '', '', 
+var currentScreen = 0, menus = ['splash' ,'tutorial page 1' ,'tutorial page 2' ,'tutorial page 3' ,'add email' ,'syncronization', '', '', '', '', 
              'contacts-compose', 'compose', 'inbox', 'sent', 'trash', 'archive', 'settings', '', '', '', 
              'reply all', 'reply', 'read message', 'forward', 'contacts-forward'];
 
-// stage: 0=new, 1=tutorial, 2=added email, 3=sync'd
-var currentUser = { id: 1, name: 'Barrack Obama', language: 'en', stage: 0 };
-var mailbox = [];
-mailbox['inbox'] = {
-			current: -1,
-			messages: []
-			};
-mailbox['sent'] =  {
-				current: -1,
-				messages: []
-			};
-mailbox['archive'] = {
-				current: -1,
-				messages: []
-			};
-mailbox['trash'] =  {
-				current: -1,
-				messages :[]
-			}
+//stage: 0=new, 1=tutorial, 2=added email, 3=sync'd
+var currentUser = { id: 0, name: '', language: 'en', stage: 0 };
 
-var contacts = [];
-var currentContact = -1;
+var mailbox = [];
+mailbox['inbox']   = { current: -1, messages: [] };
+mailbox['sent']    = { current: -1, messages: [] };
+mailbox['archive'] = { current: -1, messages: [] };
+mailbox['trash']   = { current: -1, messages: [] };
+
+var contacts = [], currentContact = -1;
+var mediaRec;
+
+
+
 
 $(document).ready(function(){
-
-	
-
-	
-	
-	
 	
 	// initialize TTS services
 	window.plugins.tts.startup(startupWin, fail);
@@ -50,6 +34,9 @@ $(document).ready(function(){
 	    },
 	    longTap: function(event, target){
 	    	longTapCustomCallback(event, target);
+	    },
+	    doubleTap: function(event, target){
+	    	doubleTapCustomCallback(event, target);
 	    },
 	    tap: function(event, target){
 	    	tapCustomCallback(event, target);	    
@@ -115,6 +102,8 @@ $(document).ready(function(){
 	function afterMenuSelect(){
 
 		if (currentScreen == 0){
+			// TODO: login and set currentUser variable
+			
 			if (currentUser.stage == 1){
 				currentScreen = 4;
 				afterMenuSelect();
@@ -133,24 +122,26 @@ $(document).ready(function(){
 		}
 
 		if (currentScreen == 4){
-			// TODO: save currentUser.stage = 1 (tutorial finished)
-			// daca a ajuns la add email, atunci data viitoare de pe splash page il duce direct pe add email
-			if (currentUser.stage == 0) currentUser.stage = 1;
+			if (currentUser.stage == 0){
+				// TODO: save currentUser.stage = 1 (tutorial finished)
+				currentUser.stage = 1;
+			}
+			if (currentUser.stage == 2){
+				currentScreen = 5;
+			}
 		}
 
 		if (currentScreen == 5){
-			// TODO: incearca credentials de pe screen 4
-			// a) daca nu sunt ok, atunci currentScreen = 4 si return (cu un notice eventual)
-			// b) daca sunt ok, atunci incepe sincronizarea si save currentUser.stage = 2 (added email)
-			// Pe callbackul de succes currentUser.stage = 3 (sync'd) si redirecteaza catre users
-			currentScreen = 12; // redirect to inbox
-			currentUser.stage = 3;
+			window.plugins.tts.speak('Synchronizing. Please wait!');
+			// TODO: metoda de a trece spre inbox cand a terminat sincronizarea
+			// poate polling getStage si daca e 3 atunci:
+			//			currentScreen = 12; // redirect to inbox
+			//			currentUser.stage = 3;
 		}
 		
 		$('.page').hide();
 		$('#page-' + currentScreen).show();
 		$('#status').html(currentScreen);
-		
 		window.plugins.tts.speak($('#page-' + currentScreen + ' .read-text').text());
 		
 		if (currentScreen == 22){
@@ -158,7 +149,8 @@ $(document).ready(function(){
 			readCurrentMessage();
 		}
 	}
-	
+
+	// TODO: adaptarea functiei pentru toate folderele
 	function afterMessageSelect(){
 		if ([12,13,14,15].indexOf(currentScreen) > -1 && mailbox['inbox'].current >= 0){
 			window.plugins.tts.speak('' + mailbox['inbox'].messages.length + ' messages. message ' + mailbox['inbox'].current);
@@ -172,12 +164,15 @@ $(document).ready(function(){
 		
 		$.ajax({
             type: "GET",
+            // TODO: mesajele trebuie aduse din emails
             url: 'http://staging.echoesapp.com/messages.json',
             dataType: 'json',
             data: {
+            	// TODO: pentru toate apelurile ajax se va folosi TOKEN in loc de userid
             	current_user: currentUser.id
             },
             success: function(response){
+            	// TODO: se aduc mesajele din toate folderele
             	mailbox['inbox'].messages = [];
             	mailbox['inbox'].current = -1;
             	response.forEach(function(element, index, array) {
@@ -189,17 +184,17 @@ $(document).ready(function(){
         		}
             },
             error: function(error) {
-           	window.plugins.tts.speak('error: ' + error.statusText);
+            	window.plugins.tts.speak('error: ' + error.statusText);
             }
         });		
 	}
 	
+	// TODO: a se seta un parametru (mesaj) pe functia asta
 	function readCurrentMessage(){
 		window.plugins.tts.speak("From:" + mailbox['inbox'].messages[mailbox['inbox'].current].from);
 		window.plugins.tts.speak("To:" + mailbox['inbox'].messages[mailbox['inbox'].current].to);
 		window.plugins.tts.speak("Subject:" + mailbox['inbox'].messages[mailbox['inbox'].current].subject);
-		window.plugins.tts.speak("Content:" + mailbox['inbox'].messages[mailbox['inbox'].current].content);		  
-
+		window.plugins.tts.speak("Content:" + mailbox['inbox'].messages[mailbox['inbox'].current].content);
 	}
 	
 	// retrieve contacts	
@@ -211,6 +206,7 @@ $(document).ready(function(){
             url: 'http://staging.echoesapp.com/contacts.json',
             dataType: 'json',
             data: {
+            	// TODO: pentru toate apelurile ajax se va folosi TOKEN in loc de userid
             	current_user: currentUser.id
             },
             success: function(response){
@@ -225,63 +221,61 @@ $(document).ready(function(){
         		}
             },
             error: function(error) {
-            	alert(error.statusText);
+            	window.plugins.tts.speak('error: ' + error.statusText);
             }
         });	
 	}
 	
-	function afterContactSelect(){
+	function afterContactSelect() {
 		if ([10,24].indexOf(currentScreen) > -1 && currentContact >= 0){
 			window.plugins.tts.speak('' + contacts.length + ' contacts. contact ' + currentContact);
-			readCurrentContact();
+			window.plugins.tts.speak("Name " + contacts[currentContact].name + ". Email " + contacts[currentContact].email);
 		}
 	}
-	
-	function readCurrentContact(){
-		window.plugins.tts.speak("Name:" + contacts[currentContact].name);
-		window.plugins.tts.speak("Email:" + contacts[currentContact].email);	  
-	}
-	
+			
 
 	$("#sync-button").click(function(){
-		 $.ajax({
-			 type: "POST",
-			 url: 'http://staging.echoesapp.com/api/updatemail.json',
-			 headers:  {'Content-Type' : 'application/json'},
-			 data: {email_address: $('#email-input').text() , email_password: $('#password-input').text()},
-			 dataType: 'json',
-			 
-			 success: function (response) {
-				 alert(response);
-				 if (response.success == true) {
-					 afterMenuSelect();
-				 }else {
-					 alert("Email or password incorrect");
-					 window.plugins.tts.speak("Email or password incorrect");
-					 }
+		$.ajax({
+			type: "POST",
+			url: 'http://staging.echoesapp.com/api/updatemail.json',
+			headers:  {'Content-Type' : 'application/json'},
+			data: {
+				// TODO: aici se mai adauga un token
+				email_address: $('#email-input').text(), 
+				email_password: $('#password-input').text()
+				},
+			dataType: 'json',
+			success: function (response) {
+				alert(response);
+				if (response.success == true) {
+					afterMenuSelect();
+				} else {
+					alert("Email or password incorrect");
+					window.plugins.tts.speak("Email or password incorrect");
+				}
 		   
-				 },
-				 error: function (error) {
-					 alert("Something went wrong");
-			 }
-		 });
+			},
+			error: function (error) {
+				alert("Something went wrong");
+			}
+		});
 	});
 
-	$("#start-record").click(function(){
-		$("#start-record").hide();
-		$("#stop-record").show();
-		$("#play-button").hide();
+	$(".start-record").click(function(){
+		$(".start-record").hide();
+		$(".stop-record").show();
+		$(".play-button").hide();
 		recordAudio();
 	});
   
-	$("#record-button").click(function(){
-		$("#stop-record").hide();
+	$(".record-button").click(function(){
+		$(".stop-record").hide();
 		stopRec();
-		$("#play-button").show();
-		$("#start-record").show();
+		$(".play-button").show();
+		$(".start-record").show();
 	});
   
-	$("#play-button").click(function(){
+	$(".play-button").click(function(){
 		playAudio();
 	});
 
@@ -291,12 +285,12 @@ $(document).ready(function(){
 	
 	
 	
-	// CALLBACKS
+	// GESTURES CALLBACKS
 	function swipeCustomCallback(event, direction, distance, duration, fingerCount) {
 		previousScreen = currentScreen;
 	  
 	    if (direction == 'left'){
-		    if([0,1,2,3,11,12,13,14,15,20,21,22,4].indexOf(currentScreen) > -1) currentScreen += 1;
+		    if([0,1,2,3,11,12,13,14,15,20,21,22].indexOf(currentScreen) > -1) currentScreen += 1;
 		    window.plugins.tts.stop(win, fail);
 		    afterMenuSelect();
 	    }
@@ -413,6 +407,11 @@ $(document).ready(function(){
     }
 	
 	
+	function doubleTapCustomCallback(event, target) {
+		window.plugins.tts.speak('double tap');
+    }
+	
+	
 	function tapCustomCallback(event, target) {
     	if ([12,13,14,15].indexOf(currentScreen) > -1){
 			window.plugins.tts.stop(win, fail);
@@ -445,109 +444,6 @@ $(document).ready(function(){
 	    	}
     	}
 	}
-	// END CALLBACKS
+	// END GESTURES CALLBACKS
 
 });
-
-//-----------------VARIABLES------------------------//
-
-var mediaRec;
-
-var randnrmsgname = Math.floor(Math.random()*1001);  // generates a random number between 1 - 1000 
-
-var currenttimestampz = new Date();
-
-var getcurrenttimez = "" + currenttimestampz.getFullYear() + "m" + currenttimestampz.getMonth() + "d" + currenttimestampz.getDate() + "h" + currenttimestampz.getHours() + "m" + currenttimestampz.getMinutes() + "s" + currenttimestampz.getSeconds();
-
-var src = "echomessage" + getcurrenttimez + "rn" + randnrmsgname + ".mp3";  // src variable (message) gets timestamp + a random number + mp3 extension as a name
-
-//--------------RECORD------------------/
-
- function recordAudio() {
-        
-        mediaRec = new Media(src, onSuccess, onError);
-        
-        // Record audio begins here
-        mediaRec.startRecord();
-		
-        // Stops recording after 30 sec (default if stopRec is not called)
-        var recTime = 0;
-        var recInterval = setInterval(function() {
-            recTime = recTime + 1;
-          
-            if (recTime >= 30) {
-                clearInterval(recInterval);
-                mediaRec.stopRecord();
-            }
-         
-        }, 1000);
-        
-    }
-//--------------STOP REC --------------//
-	function stopRec(){
-			
-			mediaRec.stopRecord();
-	}
-     
-//-------------- PLAY--------------------//
-    
-// Play audio
-
-function playAudio(srcm) {
-	var srcm = src;
-	var my_media = null;
-	var mediaTimer = null;
-    
-	if (my_media == null) {
-        // Creates Media object from srcm
-        my_media = new Media(srcm, onSuccess, onError);
-    } // else play current audio
-    	
-	// Play audio
-	my_media.play();
-
-    // Update my_media position every second
-    if (mediaTimer == null) {
-        mediaTimer = setInterval(function() {
-            // get my_media position
-            my_media.getCurrentPosition(
-                // success callback
-                function(position) {
-                    if (position > -1) {
-                        setAudioPosition((position) + " sec");
-                    }
-                },
-                // error callback
-                function(e) {
-                    console.log("Error getting pos=" + e);
-                    setAudioPosition("Error: " + e);
-                }
-            );
-        }, 1000);
-    }
-}
-
-
-
-// Cordova is ready
-//
-function onDeviceReady() {
-    recordAudio();
-}
-
-// onSuccess Callback
-//
-function onSuccess() {
-    console.log("recordAudio():Audio Success");
-}
-
-// onError Callback 
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-}
-	 
-function setAudioPosition(position) {
-    document.getElementById('audio_position').innerHTML = position;
-}
- 
