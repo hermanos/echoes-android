83fc59d08de870b9021bac74f3fc1d8b296962aa
Mir <mirosul@gmail.com>
2013-07-30 11:57:57 +0300

    cleanups

--- email-android/src/com/itnt/echoes/EchoesActivity.java
+++ email-android/src/com/itnt/echoes/EchoesActivity.java
@@ -3,7 +3,7 @@ package com.itnt.echoes;
 import org.apache.cordova.DroidGap;
 import android.os.Bundle;
 
-public class MainActivity extends DroidGap {
+public class EchoesActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
